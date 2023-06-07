e67e9025e052f7509783117060ec5d3914494e3e
Gabriel <gabrielbsornas@gmail.com>
2023-06-07 18:43:11 -0300

    main layout

--- app/src/main/java/com/documentation/fastpuc/MainActivity.kt
+++ app/src/main/java/com/documentation/fastpuc/MainActivity.kt
@@ -2,16 +2,22 @@ package com.documentation.fastpuc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.documentation.fastpuc.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding:  ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val points = listOf("H0", "H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "H11", "H12", "H13", "H14", "H15")
+        val adapter = ArrayAdapter(this, R.layout.list_item, points)
+        binding.ACTStartPoint.setAdapter(adapter)
+        binding.ACTEndPoint.setAdapter(adapter)
     }
 }
