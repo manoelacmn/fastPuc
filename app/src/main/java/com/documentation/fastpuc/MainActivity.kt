2f9e99baaefaf8048a0d3cf1a7f59bc178ce08cd
manoelacmn <manoel.acmn@puccampinas.edu.br>
2023-06-07 22:52:01 -0300

    before merge

--- app/src/main/java/com/documentation/fastpuc/MainActivity.kt
+++ app/src/main/java/com/documentation/fastpuc/MainActivity.kt
@@ -1,17 +1,36 @@
 package com.documentation.fastpuc
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.documentation.fastpuc.databinding.ActivityMainBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 
 class MainActivity : AppCompatActivity() {
 
+
     private lateinit var binding:  ActivityMainBinding
+    val db = Firebase.firestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+
+        db.collection("v1")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d(TAG, "Error getting documents: ", exception)
+            }
     }
 }
