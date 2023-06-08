2a134413c12bd61d55d6f4c9584d6910685aee76
manoelacmn <manoel.acmn@puccampinas.edu.br>
2023-06-07 23:54:48 -0300

    added adjacencyMatrix

---
+++ app/src/main/java/com/documentation/fastpuc/AdjacencyMatrix.kt
@@ -0,0 +1,78 @@
+package com.documentation.fastpuc
+
+class AdjacencyMatrix<T>: Graph<T> {
+    private val vertices = arrayListOf<vertex<T>>()
+    private val weights = arrayListOf<ArrayList<Double?>>()
+
+
+
+//    override fun createVertex(data: T): vertex<T> {
+//        val vertex = vertex(vertices.count(),data)
+//        vertices.add(vertex)
+//        weights.forEach(){
+//            it.add(null)
+//        }
+//        weights.add(arrayListOf())
+//        return vertex
+//    }
+    override fun createVertex(data: T): vertex<T> {
+        val vertex = vertex(vertices.count(), data)
+        vertices.add(vertex) // 1
+        weights.forEach {
+            it.add(null) // 2
+        }
+        val row = ArrayList<Double?>(vertices.count())
+        repeat(vertices.count()) {
+            row.add(null)
+        }
+        weights.add(row) // 3
+        return vertex
+    }
+
+    override fun edges(source: vertex<T>): ArrayList<edge<T>> {
+        val edges = arrayListOf<edge<T>>()
+        (0 until weights.size).forEach { column ->
+            val weight = weights[source.index][column]
+            if (weight != null) {
+                edges.add(edge(source, vertices[column], weight))
+            }
+        }
+        return edges
+    }
+
+    override fun addDirectedEdge(source: vertex<T>, destination: vertex<T>, weight: Double?) {
+        weights[source.index][destination.index] = weight
+    }
+
+    override fun addUndirectionedEdge(source: vertex<T>, destination: vertex<T>, weight: Double?) {
+        addDirectedEdge(source,destination,weight)
+        addDirectedEdge(destination,source,weight)
+    }
+
+
+    override fun weight(source: vertex<T>, destination: vertex<T>): Double? {
+        return weights[source.index][destination.index]
+    }
+
+    override fun toString(): String {
+// 1
+        val verticesDescription = vertices
+            .joinToString(separator = "\n") { "${it.index}: ${it.data}" }
+// 2
+                val grid = weights.map { row ->
+                    buildString {
+                        (0 until weights.size).forEach { columnIndex ->
+                            val value = row[columnIndex]
+                            if (value != null) {
+                                append("$value\t")
+                            } else {
+                                append("ø\t\t")
+                            }
+                        }
+                    }
+                }
+                val edgesDescription = grid.joinToString("\n")
+// 3
+                return "$verticesDescription\n\n$edgesDescription"
+            }
+}
--- app/src/main/java/com/documentation/fastpuc/MainActivity.kt
+++ app/src/main/java/com/documentation/fastpuc/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.documentation.fastpuc.databinding.ActivityMainBinding
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -20,6 +21,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val points = listOf("H0", "H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "H11", "H12", "H13", "H14", "H15")
+
+
+        val graph = AdjacencyMatrix<String>()
+        for (i in points){
+            graph.createVertex(i);
+        }
+
+
+
 
 
         db.collection("v1")
@@ -33,9 +44,12 @@ class MainActivity : AppCompatActivity() {
                 Log.d(TAG, "Error getting documents: ", exception)
             }
 
-        val points = listOf("H0", "H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "H11", "H12", "H13", "H14", "H15")
         val adapter = ArrayAdapter(this, R.layout.list_item, points)
         binding.ACTStartPoint.setAdapter(adapter)
         binding.ACTEndPoint.setAdapter(adapter)
+
+        binding.btnProcurar.setOnClickListener { Toast.makeText(this, binding.ACTStartPoint.text, Toast.LENGTH_SHORT).show() }
+
+
     }
 }
