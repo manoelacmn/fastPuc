753ffc191d547d377cc071fc49336686cdd9e5b6
SiedBaral <carlosbaral236@gmail.com>
2023-06-12 23:08:55 -0300

    Formatação do tempo
    Escrevendo em Horas:Minutos:segundos

--- app/src/main/java/com/documentation/fastpuc/MainActivity.kt
+++ app/src/main/java/com/documentation/fastpuc/MainActivity.kt
@@ -22,20 +22,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        val points = listOf("H0", "H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "H11", "H12", "H13", "H14", "H15")
-//        val points = listOf<String>()
-
 
         val graph = AdjacencyMatrix<String>()
 
-//        val h0 = graph.createVertex("H0")
-//        val h1 = graph.createVertex("H1")
-//        val h2 = graph.createVertex("H2")
-//        val h3 = graph.createVertex("H3")
-//        val h4 = graph.createVertex("H4")
-//
-//
-//        graph.addUndirectionedEdge(h0, h4, 1000.00)
 
         println(graph.toString())
 
@@ -56,7 +45,7 @@ class MainActivity : AppCompatActivity() {
 
                     if(alreadyExists==null){
                         val first = graph.createVertex(document.id)
-                        for ((key, value) in document.data) { // acess the others values within a document
+                        for ((key, value) in document.data) {
                               Log.d("NEW DOCUMENT",value.toString())
 
                             if(key!="name"){
@@ -66,22 +55,19 @@ class MainActivity : AppCompatActivity() {
                                 val alreadyExists1 = graph.vertices.find { it.data ==  key}
                                 Log.d("ALREADY EXIST1:","${alreadyExists1.toString()} VALUE: ${value.toString()}")
 
-                                if (alreadyExists1 == null){ // if not exists then create a vertice and its edge
+                                if (alreadyExists1 == null){
                                     println(graph.toString())
                                     val others= graph.createVertex(key)
                                     graph.add(EdgeType.UNDIRECTED,first, others, value.toString().toDouble())
-                                }else{  // if exists only create edge
+                                }else{
                                     graph.add(EdgeType.UNDIRECTED,first, alreadyExists1, value.toString().toDouble())
                                 }
 
-                               // val others= graph.createVertex(key)
-                                // acess the others values within a document
-                                 // create edges with the others
                             }
 
                         }
                     }else{
-                        for ((key, value) in document.data) { // acess the others values within a document
+                        for ((key, value) in document.data) { 
                             Log.d("NEW DOCUMENT",value.toString())
 
                             if(key!="name"){
@@ -91,17 +77,15 @@ class MainActivity : AppCompatActivity() {
                                 val alreadyExists1 = graph.vertices.find { it.data ==  key}
                                 Log.d("ALREADY EXIST1:","${alreadyExists1.toString()} VALUE: ${value.toString()}")
 
-                                if (alreadyExists1 == null){ // if not exists then create a vertice and its edge
+                                if (alreadyExists1 == null){
                                     println(graph.toString())
                                     val others= graph.createVertex(key)
                                     graph.add(EdgeType.UNDIRECTED,alreadyExists, others, value.toString().toDouble())
-                                }else{  // if exists only create edge
+                                }else{
                                     graph.add(EdgeType.UNDIRECTED,alreadyExists, alreadyExists1, value.toString().toDouble())
                                 }
 
-                                // val others= graph.createVertex(key)
-                                // acess the others values within a document
-                                // create edges with the others
+
                             }
 
                         }
@@ -109,31 +93,17 @@ class MainActivity : AppCompatActivity() {
 
 
                     if (!points.contains(document.data["name"])){
-                        points.add(document.data["name"] as String) // verify if vertice name is already contained if not it adds
-                        // points.add(key)
+                        points.add(document.data["name"] as String) 
                     }
-//                    print(index.toString())
+
                     Log.d("INDEX HASHMAP",index.toString())
                     for ((key, value) in index) {
-                        //  Log.d("NEW DOCUMENT","DOCUMENT")
+
                         println("MAP DATA:  $key: $value")
 
                     }
                 }
-//                println(graph.toString())
-//                Log.d("GRAPH VALUES",graph.toString())
-//
-//                val vertexA = graph.vertices.find { it.data ==  "a"}!!
-//
-//                val vertexD = graph.vertices.find { it.data ==  "f"}!!
-//                Log.d("FINAL VERTEX:",vertexD.toString())
-//                val shortestPath = graph.shortestPath(vertexA, vertexD)
-//
-//                if (shortestPath.isNotEmpty()) {
-//                    println("Shortest path from A to D: ${shortestPath.joinToString(" -> ") { it.data }}")
-//                } else {
-//                    println("No path exists")
-//                }
+
 
             }
             .addOnFailureListener { exception ->
@@ -142,39 +112,11 @@ class MainActivity : AppCompatActivity() {
 
         Log.d("FINAL LIST",points.toString())
 
-//        println(graph.toString())
-
-//
-//        println("HERE")
-
-//        val vertexA = graph.vertices.find { it.data ==  "a"}!!
-//
-//        val vertexD = graph.vertices.find { it.data ==  "f"}!!
-//        val shortestPath = graph.shortestPath(vertexA, vertexD)
-//
-//        if (shortestPath.isNotEmpty()) {
-//            println("Shortest path from A to D: ${shortestPath.joinToString(" -> ") { it.data }}")
-//        } else {
-//            println("No path exists")
-//        }
 
 
         println("HERE")
         print(index.toString())
 
-//        val vertexA = graph.createVertex("uwu")
-//        val vertexB = graph.createVertex("uwu1")
-//        val vertexC = graph.createVertex("uwu2")
-//        val vertexD = graph.createVertex("uwu3")
-//
-//        graph.addDirectedEdge(vertexA, vertexB, 5.0)
-//        graph.addDirectedEdge(vertexA, vertexC, 3.0)
-//        graph.addDirectedEdge(vertexB, vertexD, 2.0)
-//        graph.addDirectedEdge(vertexC, vertexD, 1.0)
-//
-//        val shortestPath = graph.shortestPath(vertexA, vertexD)
-
-//        Log.d("SHORTEST PATH",shortestPath.toString())
 
         val adapter1 = ArrayAdapter(this, R.layout.list_item, points)
         val adapter2 = ArrayAdapter(this, R.layout.list_item, points)
@@ -185,13 +127,6 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, binding.ACTStartPoint.text, Toast.LENGTH_SHORT).show()
             Log.d("STARTING POINT", binding.ACTStartPoint.text.toString())
             Log.d("ENDING POINT POINT", binding.ACTEndPoint.text.toString())
-
-//            val startVertex = graph.vertices.find { it.data ==  binding.ACTStartPoint.text.toString()}
-            //val endvVertex = graph.vertices.find { it.data ==  binding.ACTEndPoint.text.toString()}
-
-           // val endvVertex = graph.vertices.find { index[binding.ACTEndPoint.text.toString()].toString()}
-
-
 
             val tempKey = index[binding.ACTStartPoint.text.toString()]
 
@@ -215,31 +150,26 @@ class MainActivity : AppCompatActivity() {
                 Log.d("Edge", "From: ${edge.source.data} To: ${edge.destination} Weight: ${edge.weight}")
 
             }
-//            graph.dijkstraShortestPath(endVertex!!)
+
             Log.d("SHORTEST PATH",graph.dijkstraShortestPath(endVertex!!).toString())
 
             val  smallestPaths = graph.dijkstraShortestPath(endVertex)
 
             for((vertex,weight) in smallestPaths){
                 if (vertex.data == tempKey) {
-                    val format  = DecimalFormat("#.##")
-                    val result = (weight.toDouble()/1.4)
-                    val text = format.format(result)
+                    val weightInSeconds = weight.toDouble() / 1.4
+                    val hours = weightInSeconds / 3600
+                    val minutes = (weightInSeconds % 3600) / 60
+                    val seconds = weightInSeconds % 60
+
+                    val formattedTime = String.format("%02d:%02d:%02d", hours.toInt(), minutes.toInt(), seconds.toInt())
+
                     println("Vertex: $vertex, Weight: $weight")
-                    Log.d("Total time:", (text.toString()));
-                    binding.tvResult.text = text
+                    Log.d("Total time:", formattedTime)
+                    binding.tvResult.text = formattedTime
                 }
             }
 
-//            if (shortestDistances != null) {
-//                for (vertex in shortestDistances.keys) {
-//                    val distance = shortestDistances.get(vertex)
-//                    println("Shortest distance from A to ${vertex.data}: $distance")
-//                    if (distance != null) {
-//                        Log.d("SHORTEST PATH",(distance/1.6).toString())
-//                    }
-//                }
-//            }
 
             Log.d("VERTICES LIST:",graph.vertices.toString())
 
